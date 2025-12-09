6efcd405f9a28fb5fac7707fde5a8ae8b1d13352
Maksim Aksh <yaposhka277@gmail.com>
2025-12-10 01:52:30 +0300

    initial commit

---
+++ src/main/java/ru/aksh/pdfconversion/PdfConversionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.aksh.pdfconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfConversionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfConversionMicroserviceApplication.class, args);
+	}
+
+}
